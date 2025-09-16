e9475731dbbf4220bf85aac750192b2f88c7462b
ryn <74331860+zzryn@users.noreply.github.com>
2025-09-16 12:59:35 -0400

    Remove extension-only script

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.action.onClicked.addListener((tab) => {
-  if (tab.url.includes("docs.google.com/forms") && !tab.url.includes("edit")) {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ["content.js"]
-    });
-  }
-});
